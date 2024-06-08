b9a673fff05c78ec3808e5ae82c4f07ae6de4b75
Suvrat Apte <suvratapte@gmail.com>
2024-06-08 11:24:41 +0200

    Improve the basic implementation

--- src/game_theory/basic.clj
+++ src/game_theory/basic.clj
@@ -1,117 +1,116 @@
 (ns game-theory.basic)
 
 (def initial-state
-  {:first-player []
-   :second-player []
+  {:first-player-moves []
+   :second-player-moves []
    :score {:first-player 0
            :second-player 0}})
 
 
 (defn- grudger
-  [current-state player-key opponent-key]
-  (let [opponent-moves (opponent-key current-state)]
-    (if (some #(= :de %) opponent-moves)
-      :de
-      :co)))
+  [player-moves opponent-moves]
+  (if (some #(= :de %) opponent-moves)
+    :de
+    :co))
 
 
 (defn- not-so-simple
-  [current-state player-key opponent-key]
+  [player-moves opponent-moves]
   (if (> (rand-int 10) 2)
     :de
     :co))
 
 
 (defn- simple
-  [current-state player-key opponent-key]
+  [player-moves opponent-moves]
   :co)
 
 
 (defn- devil
-  [current-state player-key opponent-key]
+  [player-moves opponent-moves]
   :de)
 
 
 (defn- tit-for-tat
-  [current-state player-key opponent-key]
-  (let [opponent-moves (opponent-key current-state)]
-    (or (last opponent-moves) :co)))
+  [player-moves opponent-moves]
+  (or (last opponent-moves) :co))
+
+
+(defn- tit-for-two-tats
+  [player-moves opponent-moves]
+  (let [last-move (last opponent-moves)
+        second-last-move (-> opponent-moves
+                             drop-last
+                             last)]
+    (if (and (= last-move :de)
+             (= second-last-move :de))
+      :de
+      :co)))
 
 
 (defn- joss
-  [current-state player-key opponent-key]
-  (let [opponent-moves (opponent-key current-state)]
-    (if (> (rand-int 10) 2)
-      :de
-      (or (last opponent-moves) :co))))
+  [player-moves opponent-moves]
+  (if (> (rand-int 10) 2)
+    :de
+    (or (last opponent-moves) :co)))
 
 
-(defn- compute-state
-  [current-state first-player-move second-player-move]
-  (let [state-with-updated-moves
-        {:first-player (conj (:first-player current-state)
-                             first-player-move)
-         :second-player (conj (:second-player current-state)
-                              second-player-move)}]
-    (cond
+(defn- compute-score
+  "Returns the score in this format:
+   [<first-player-score> <second-player-score>]"
+  [first-player-move second-player-move]
+  (cond
       ;; Both cooperate
       (and (= :co first-player-move)
            (= :co second-player-move))
-      (assoc state-with-updated-moves
-             :score {:first-player (-> current-state
-                                       :score :first-player
-                                       (+ 3))
-                     :second-player (-> current-state
-                                        :score :second-player
-                                        (+ 3))})
+      [3 3]
 
       ;; Both defect
       (and (= :de first-player-move)
            (= :de second-player-move))
-      (assoc state-with-updated-moves
-             :score {:first-player (-> current-state
-                                       :score :first-player
-                                       (+ 1))
-                     :second-player (-> current-state
-                                        :score :second-player
-                                        (+ 1))})
+      [1 1]
 
       ;; One cooperatiion one defection
       (and (= :co first-player-move)
            (= :de second-player-move))
-      (assoc state-with-updated-moves
-             :score {:first-player (-> current-state
-                                       :score :first-player
-                                       (+ 0))
-                     :second-player (-> current-state
-                                        :score :second-player
-                                        (+ 5))})
+      [0 5]
 
       (and (= :de first-player-move)
            (= :co second-player-move))
-      (assoc state-with-updated-moves
-             :score {:first-player (-> current-state
-                                       :score :first-player
-                                       (+ 5))
-                     :second-player (-> current-state
-                                        :score :second-player
-                                        (+ 0))}))))
+      [5 0]))
 
 
 (defn- simulate-game
   [rounds-number first-player-stragey second-player-stragey]
   (loop [state initial-state
          n rounds-number]
-    (let [first-player-move (first-player-stragey state
-                                                  :first-player
-                                                  :second-player)
-          second-player-move (second-player-stragey state
-                                                    :second-player
-                                                    :first-player)
-          new-state (compute-state state
-                                   first-player-move
-                                   second-player-move)
+    (let [first-player-moves (:first-player-moves state)
+          second-player-moves (:second-player-moves state)
+          first-player-move (first-player-stragey first-player-moves
+                                                  second-player-moves)
+          second-player-move (second-player-stragey second-player-moves
+                                                    first-player-moves)
+          [first-player-score second-player-score]
+          (compute-score first-player-move
+                         second-player-move)
+
+          new-state (-> state
+                        (update :first-player-moves conj first-player-move)
+                        (update :second-player-moves conj second-player-move)
+                        (update-in [:score :first-player] + first-player-score)
+                        (update-in [:score :second-player] + second-player-score))
+
           n (dec n)]
       (if (pos? n)
         (recur new-state n)
         new-state))))
+
+(comment
+
+  (simulate-game 10 joss tit-for-tat)
+
+  ;; ---
+
+  (-> 10
+      (simulate-game joss tit-for-tat)
+      :score))
